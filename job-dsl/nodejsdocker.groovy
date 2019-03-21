8014b27d342b63ca56b15b6706845b58c1726b33
devbuck <45263987+devbuck@users.noreply.github.com>
2019-03-21 12:32:58 +0000

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -16,7 +16,7 @@ job('NodeJS Docker example') {
         dockerBuildAndPublish {
             repositoryName('devbuck/docker-demo')
             tag('${GIT_REVISION,length=9}')
-            registryCredentials('dockerhub') // this is the user id credentials setup on Jenkins console
+            registryCredentials('devbuck') // this is the user id credentials setup on Jenkins console
             forcePull(false)
             forceTag(false)
             createFingerprints(false)
